6b4fb060d981750ae1988dacb5ac3d03797c9456
newt <ysti2019.win2020.st@gmail.com>
2025-04-19 17:39:01 +0900

    fix: slide list page

---
+++ home/app/routes/home.tsx
@@ -0,0 +1,34 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "Slides | newt239.dev" }];
+}
+
+export async function loader() {
+  const slides = [{ id: "example", title: "Welcome to Slidev" }];
+
+  return { slides };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { slides } = loaderData;
+  return (
+    <>
+      <main>
+        <h1>slides.newt239.dev</h1>
+        <ul>
+          {slides.map((slide) => (
+            <li key={slide.id}>
+              <a href={`/slides/${slide.id}`}>{slide.title}</a>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer>
+        <small>
+          Copyright 2025 <a href="https://newt239.dev">newt239.dev</a>
+        </small>
+      </footer>
+    </>
+  );
+}
--- home/vite.config.ts
+++ home/vite.config.ts
@@ -1,11 +1,7 @@
+import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "../dist",
-    emptyOutDir: false,
-  },
+  plugins: [reactRouter(), tsconfigPaths()],
 });
